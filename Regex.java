f7ab57ec64ade431273a8f4c53fcb4c7dc07e15f
priyanshi1208 <pri.shukla2208@gmail.com>
2021-05-27 15:30:57 +0530

    Added lastname

--- Regex.java
+++ Regex.java
@@ -5,12 +5,17 @@ import java.util.regex.Pattern;
 public class Regex {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        String firstName;
+        String firstName,lastName;
         do {
             System.out.println("Enter first name");
             firstName = scanner.next();
             firstName(firstName);
         }while(!firstName(firstName));
+        do {
+            System.out.println("Enter last name");
+            lastName = scanner.next();
+            lastName(lastName);
+        }while(!firstName(lastName));
     }
     public static boolean regexCheck(String reg, String input){
         Pattern compile = Pattern.compile(reg);
@@ -22,5 +27,9 @@ public class Regex {
         String reg="^[A-Z][a-z]{3,}";
         return regexCheck(reg,firstName);
    }
+    public static boolean lastName(String lastName) {
+        String reg = "^[A-Z][a-z]{3,}";
+        return regexCheck(reg, lastName);
+    }
 
 }
